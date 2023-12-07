b403fe0041e9ed599777e43f0d18f81e2fed2ba3
Adam Patni <adampatni27@gmail.com>
2023-12-07 13:26:21 -0500

    truncated SVD + experiment fixing

--- generate_A.py
+++ generate_A.py
@@ -1,10 +1,12 @@
 import numpy as np
 
+
 def nonnegative_matrices(n, size=(100, 100)):
     out = np.zeros((n, *size))
     for i in range(n):
         out[i] = np.random.rand(*size)
     return out
+
 
 def positive_matrices(n, size=(100, 100)):
     out = np.zeros((n, *size))
@@ -12,18 +14,21 @@ def positive_matrices(n, size=(100, 100)):
         out[i] = 1 + np.random.rand(*size)
     return out
 
+
 def random_distribution_matrices(n, size=(100, 100)):
     out = np.zeros((n, *size))
     for i in range(n):
         out[i] = np.random.uniform(-1, 1, size)
     return out
 
+
 def symmetric_matrices(n, size=(100, 100)):
     out = np.zeros((n, *size))
     for i in range(n):
         b = np.random.rand(*size)
-        out[i] = b+b.T/2
+        out[i] = b + b.T / 2
     return out
+
 
 def spd_matrices(n, size=(100, 100)):
     out = np.zeros((n, *size))
@@ -34,16 +39,25 @@ def spd_matrices(n, size=(100, 100)):
 
 
 # def specific_spectrum_matrices(n, size=(100, 100)):
-#     pass  # Placeholder 
+#     pass  # Placeholder
 
 # def sparsity_pattern_matrices(n, size=(100, 100)):
 #     pass # Placeholder
 
+
 # Generating and saving the matrices
-out = []
-n_matrices = 1000
-size = (100,100)
-for func in [nonnegative_matrices, positive_matrices, random_distribution_matrices, symmetric_matrices, spd_matrices]:
-    matrices = func(n_matrices, size)
-    filename = f"{func.__name__}.npy"
-    np.save(f'matrices/{filename}', matrices)
+def generate_matrices(
+    n_matrices=10,
+    size=(5, 5),
+    funcs=[
+        nonnegative_matrices,
+        positive_matrices,
+        random_distribution_matrices,
+        symmetric_matrices,
+        spd_matrices,
+    ],
+):
+    for func in funcs:
+        matrices = func(n_matrices, size)
+        filename = f"{func.__name__}.npy"
+        np.save(f"matrices/{filename}", matrices)
